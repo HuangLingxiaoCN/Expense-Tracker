014a01c1917ff5619245e6157a73e580d7d97453
Lingxiao Huang <jasonhlx1213@gmail.com>
2021-06-15 22:58:06 +0300

    adding props to reuse component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 659.42,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e2",
+      title: "Bicycle",
+      amount: 60.0,
+      date: new Date(2021, 5, 4),
+    },
+    {
+      id: "e3",
+      title: "Inspection",
+      amount: 190.0,
+      date: new Date(2020, 4, 15),
+    },
+    {
+      id: "e4",
+      title: "Washing Machine",
+      amount: 100.0,
+      date: new Date(2019, 4, 20),
+    },
+  ];
+
   return (
     <div className="App">
       <h1>Let's learn React!</h1>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
